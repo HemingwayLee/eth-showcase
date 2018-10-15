5002fca17fb117a8d920a357861086dc84727b4f
LeeHemingway <coherence0815@gmail.com>
2018-10-15 22:53:16 +0900

    Init dotenv

--- basic-operations/index.js
+++ basic-operations/index.js
@@ -1,9 +1,11 @@
 const Web3 = require('web3');
-const w3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/M1QDKJ5S7583XBFF813PTT2C6X5QHN9ANQ'));
 
 require('dotenv').config();
 
 async function main() {
+  const url = 'https://ropsten.infura.io/' + process.env.API_KEY;
+  const w3 = new Web3(new Web3.providers.HttpProvider(url));
+  
   w3.eth.getBlockNumber().then(console.log);
   console.log(await w3.eth.getBlockNumber());
 
