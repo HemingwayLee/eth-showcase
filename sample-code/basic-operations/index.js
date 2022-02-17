be3b53d040983e1c6c11859753081cbe837de808
LeeHemingway <coherence0815@gmail.com>
2022-02-18 00:12:57 +0900

    update example

--- sample-code/basic-operations/index.js
+++ sample-code/basic-operations/index.js
@@ -3,9 +3,10 @@ const Web3 = require('web3');
 require('dotenv').config();
 
 async function main() {
-  const url = 'https://ropsten.infura.io/' + process.env.API_KEY;
-  const w3 = new Web3(new Web3.providers.HttpProvider(url));
-  
+  // const url = 'https://ropsten.infura.io/' + process.env.API_KEY;
+  // const w3 = new Web3(new Web3.providers.HttpProvider(url));
+  const w3 = new Web3("https://cloudflare-eth.com")  
+
   w3.eth.getBlockNumber().then(console.log);
   console.log(await w3.eth.getBlockNumber());
 
@@ -24,3 +25,4 @@ async function main() {
 }
 
 main();
+
