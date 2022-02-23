ae04a4f43e48657f6e6604e89739a47a1ce877c4
LeeHemingway <coherence0815@gmail.com>
2022-02-23 15:13:24 +0900

    refactoring

--- sample-code/smart-contract/is-contract/index.js
+++ sample-code/smart-contract/is-contract/index.js
@@ -3,22 +3,21 @@ const Web3 = require('web3');
 require('dotenv').config();
 
 async function main() {
-  const url = 'https://ropsten.infura.io/' + process.env.API_KEY;
+  const url = 'https://ropsten.infura.io/v3/' + process.env.PROJECT_ID;
   const w3 = new Web3(new Web3.providers.HttpProvider(url));
-  
-  const contractAddr = "0xE0B60FADEe5CB82A8f55ddb7c23fBE06e9EDF1A7";
+  // const w3 = new Web3("https://cloudflare-eth.com")  
+
+  const contractAddr = "0x1a4e87768D1f2afBd8912Bcee9D58c929265d8fa";
   [contractAddr, process.env.PUBLIC_KEY].forEach(
     function(addr) {
-      
-    console.log("address", addr);
-
-    w3.eth.getCode(addr, function(error, result) {
-      if (error) {
-        console.log("error", error);
-      } else {
-        console.log("result", result);
-      }
-    });
+      w3.eth.getCode(addr, function(error, result) {
+        console.log("address", addr);
+        if (error) {
+          console.log("error", error);
+        } else {
+          console.log("result", result);
+        }
+      });
   });
 }
 
