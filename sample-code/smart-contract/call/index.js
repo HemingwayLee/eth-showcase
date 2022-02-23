b54c9157359bcd4d5380dd935424bdc948a30ba2
LeeHemingway <coherence0815@gmail.com>
2022-02-23 15:43:29 +0900

    refacotring

--- sample-code/smart-contract/call/index.js
+++ sample-code/smart-contract/call/index.js
@@ -4,12 +4,12 @@ const Web3 = require('web3');
 require('dotenv').config();
 
 async function main() {
-  const url = 'https://ropsten.infura.io/' + process.env.API_KEY;
+  const url = 'https://ropsten.infura.io/v3/' + process.env.PROJECT_ID;
   const w3 = new Web3(new Web3.providers.HttpProvider(url));
-
+  
   const abi = JSON.parse(fs.readFileSync('abi.json', 'utf8'));
 
-  const contractAddr = "0xE0B60FADEe5CB82A8f55ddb7c23fBE06e9EDF1A7";
+  const contractAddr = "0x1a4e87768D1f2afBd8912Bcee9D58c929265d8fa";
   let contract = new w3.eth.Contract(abi, contractAddr);
   
   //call and send
