8a255d81337b930ccd518a4b948e017e0a163bbc
LeeHemingway <coherence0815@gmail.com>
2022-03-09 21:57:04 +0900

    add access to geth

--- sample-code/basic-operations/index.js
+++ sample-code/basic-operations/index.js
@@ -6,11 +6,13 @@ async function main() {
   const url = 'https://ropsten.infura.io/v3/' + process.env.PROJECT_ID;
   const w3 = new Web3(new Web3.providers.HttpProvider(url));
   // const w3 = new Web3("https://cloudflare-eth.com")  
+  // const w3 = new Web3("http://localhost:8545");
 
   w3.eth.getBlockNumber().then(console.log);
   console.log(await w3.eth.getBlockNumber());
 
   w3.eth.getBalance(process.env.PUBLIC_KEY).then(console.log);
+  // w3.eth.getBalance('0x0000000000000000000000000000000000000001').then(console.log);
   w3.eth.getBalance(process.env.PUBLIC_KEY, function(err, res) {
     if (err) {
       console.log(err);
