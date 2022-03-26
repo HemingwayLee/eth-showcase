711546214e773703a87f64f20479c54bbdd22b6f
LeeHemingway <coherence0815@gmail.com>
2022-03-26 23:41:09 +0900

    test new address

--- sample-code/smart-contract/nft/openzeppelin/operations/index.js
+++ sample-code/smart-contract/nft/openzeppelin/operations/index.js
@@ -7,8 +7,12 @@ async function main() {
   const url = 'https://ropsten.infura.io/v3/' + process.env.PROJECT_ID;
   const w3 = new Web3(new Web3.providers.HttpProvider(url));
   
-  const abi = JSON.parse(fs.readFileSync('LeeYunWei.abi', 'utf8'));
-  const contractAddr = "0x1828e0ffc058b961b8d06686bcbd4582826087ae";
+  const abi = JSON.parse(fs.readFileSync('LeeYunWei2.abi', 'utf8'));
+  
+  // This is ERC-721 extension
+  const contractAddr = "0x0d509954fd1044ff7c3568e2be579B409b7122f5"
+  
+  // const contractAddr = "0x1828e0ffc058b961b8d06686bcbd4582826087ae";
   
   w3.eth.accounts.wallet.add(process.env.PRIVATE_KEY);
 
@@ -63,6 +67,17 @@ async function main() {
       console.log(err);
     } else {
       console.log("The result of symbol() is ", res);
+    }
+  })
+
+  // only ERC721 extension have totalSupply
+  contract.methods.totalSupply().call({
+    from: process.env.PUBLIC_KEY
+  }, function(err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("The result of totalSupply() is ", res);
     }
   })
 }
