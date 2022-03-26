9f82ee5604204d8e4b6af5647f1ccc997fa069ec
LeeHemingway <coherence0815@gmail.com>
2022-03-26 17:47:10 +0900

    complete the sol code

--- sample-code/smart-contract/nft/openzeppelin/solidity/erc721Extension/my2ndnft.sol
+++ sample-code/smart-contract/nft/openzeppelin/solidity/erc721Extension/my2ndnft.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 import "./node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -6,13 +7,13 @@ import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "./node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract LeeYunWei2 is ERC721URIStorage, ERC721Enumerable, Ownable {
+contract LeeYunWei2 is ERC721, ERC721URIStorage, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() ERC721("LeeYunWei2", "YWLEE2") {}
 
-    function mintNFT(address recipient, string memory tokenURI)
+    function mintNFT(address recipient, string memory metaDataJson)
         public
         onlyOwner
         returns (uint256)
@@ -21,11 +22,42 @@ contract LeeYunWei2 is ERC721URIStorage, ERC721Enumerable, Ownable {
 
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, metaDataJson);
 
         return newItemId;
     }
 
-    
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
 }
 
